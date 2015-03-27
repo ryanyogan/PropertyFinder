ad8c1b654fdcc7f3d4354fd78dd0e0627471d981
Ryan Yogan <ryanyogan@gmail.com>
2015-03-26 20:53:21 -0500

    Starting out with no JSX saying hello

--- PropertyFinderApp.js
+++ PropertyFinderApp.js
@@ -1,2 +1,20 @@
 'use strict';
 
+var React = require('react-native');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return React.createElement(React.Text, {style: styles.text}, "Hello World from non JSX!");
+  }
+}
+
+React.AppRegistry.registerComponent('PropertyFinderApp', function() { return PropertyFinderApp });
